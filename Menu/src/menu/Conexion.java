9df950a73ec6976bdb2d7b13618deb7fd193c0e5
A-cano <142453582+A-cano@users.noreply.github.com>
2025-05-25 20:51:49 -0500

    Update Conexion.java

--- Menu/src/menu/Conexion.java
+++ Menu/src/menu/Conexion.java
@@ -4,10 +4,24 @@
  */
 package menu;
 
-/**
- *
- * @author JAIME
- */
 public class Conexion {
-    
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/escuela-taller";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Alejandro1109663189"; 
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println(" Conexión establecida con éxito");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Error: Driver no encontrado");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("Error de conexión a la base de datos");
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
